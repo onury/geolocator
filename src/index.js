7b0b6330cc83ce1ed30f8ef60bf44ef1af75cb1a
Onur Yıldırım <onur@cutepilot.com>
2017-03-11 08:20:23 +0300

    fix issue #42

--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
 import geolocator from './core/geolocator';
+
+// See https://github.com/onury/geolocator/issues/42
+if (typeof window !== 'undefined'
+        && typeof window.geolocator === 'undefined') {
+    window.geolocator = geolocator;
+}
+
 // export default geolocator;
 // http://stackoverflow.com/a/33683495/112731
 module.exports = geolocator;
