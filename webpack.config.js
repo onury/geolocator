a279c8b514d451b42924381fa42efd2d869fe812
Onur Yıldırım <onur@cutepilot.com>
2018-04-20 23:23:40 +0300

    update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,58 @@
-var webpack = require('webpack');
+const path = require('path');
+const webpack = require('webpack');
 
-// Webpack configuration
-// http://webpack.github.io/docs/configuration.html
-module.exports = function (params) {
-    var main = {
+module.exports = (env) => {
+
+    const LIB_NAME = 'geolocator';
+    const SRC_PATH = path.resolve(__dirname, 'src');
+    const OUT_PATH = path.resolve(__dirname, 'dist');
+    const ENTRY_PATH = path.resolve(__dirname, 'src');
+    const PUBLIC_PATH = 'dist/';
+
+    // Webpack configuration
+    // http://webpack.github.io/docs/configuration.html
+    const config = {
+        mode: 'development',
         cache: false,
-        entry: params.entryPath,
+        entry: ENTRY_PATH,
         // http://webpack.github.io/docs/configuration.html#devtool
         devtool: 'source-map',
+        // the environment in which the bundle should run changes chunk loading
+        // behavior and available modules
         target: 'web',
+        // string (absolute path!). the home directory for webpack the entry and
+        // module.rules.loader option is resolved relative to this directory
+        context: __dirname,
         output: {
-            path: params.outPath,
-            filename: params.libFileName + '.js',
-            library: params.libName,
+            // the target directory for all output files must be an absolute
+            // path (use the Node.js path module)
+            path: OUT_PATH,
+            // the filename template for entry chunks
+            filename: LIB_NAME + '.js',
+            // the name of the exported library
+            library: LIB_NAME,
+            // the type of the exported library
             libraryTarget: 'umd',
+            // use a named AMD module in UMD library
             umdNamedDefine: true,
-            publicPath: params.publicPath
+            // the url to the output directory resolved relative to the HTML
+            // page
+            publicPath: PUBLIC_PATH
         },
         module: {
             rules: [
                 {
-                    test: /(\.jsx|\.js)$/,
-                    loader: 'babel-loader',
-                    query: {
-                        presets: ['es2015']
-                        // , plugins: [
-                        //     ['babel-plugin-transform-builtin-extend', {
-                        //         globals: ['Error', 'Array']
-                        //     }]
-                        // ]
-                    },
-                    exclude: /(node_modules|bower_components)/
+                    test: /\.js$/,
+                    exclude: /(node_modules|bower_components)/,
+                    use: {
+                        loader: 'babel-loader',
+                        options: {
+                            // https://github.com/rollup/rollup-plugin-babel/issues/120#issuecomment-301325496
+                            presets: [
+                                ['es2015', { "modules": false }]
+                            ]
+                        }
+                    }
                 },
                 {
                     test: /\.html?$/,
@@ -43,76 +65,75 @@ module.exports = function (params) {
             ]
         },
         resolve: {
-            modules: [params.srcPath],
+            modules: [SRC_PATH],
             extensions: ['.js']
         },
-        // Whether to show progress. Defaults to `true`.
-        progress: false,
-        // Configure the console output.
-        stats: {
-            colors: true,
-            modules: false,
-            reasons: true
-        },
-        // Whether to report error to grunt if webpack find errors. Use
-        // this if webpack errors are tolerable and grunt should
-        // continue.
-        failOnError: true,
-        // Use webpacks watcher. Requires the grunt process
-        // to be kept alive.
-        watch: false,
-        // Whether to finish the grunt task. Use this in combination
-        // with the watch option.
-        keepalive: false,
-        // Whether to embed the webpack-dev-server runtime into the
-        // bundle. Defaults to `false`.
-        inline: false
-    };
-
-    return {
-        main: main,
-        // examples:
-        // https://github.com/webpack/webpack-with-common-libs/blob/master/Gruntfile.js
-        // http://www.christianalfoni.com/articles/2015_04_19_The-ultimate-webpack-setup
-        server: {
-            options: {
-                webpack: main,
-                publicPath: '/' + params.publicPath
-                // , headers:{
-                //     'Access-Control-Allow-Origin': '*'
-                // }
-            },
-            start: {
-                keepAlive: true,
-                host: params.host,
-                port: params.port,
-                webpack: {
-                    entry: [
-                        // The script refreshing the browser on none-hot updates
-                        'webpack-dev-server/client?' + params.baseURL,
-                        // For hot style updates
-                        'webpack/hot/dev-server',
-                        // test example html
-                        params.pagePath,
-                        // NOTE: Always the last one is exported.
-                        // source entry path
-                        params.entryPath
-                    ],
-                    devtool: 'eval',
-                    debug: true,
-                    watch: true,
-                    quite: false,
-                    noInfo: true,
-                    inline: true,
-                    historyApiFallback: true,
-                    plugins: [
-                        // Whether to add the HotModuleReplacementPlugin and
-                        // switch the server to hot mode. Use this in
-                        // combination with the inline option.
-                        new webpack.HotModuleReplacementPlugin()
-                    ]
-                }
+        // errors-only | minimal | none | normal | verbose
+        stats: 'normal',
+        plugins: [
+            // https://webpack.js.org/plugins/banner-plugin/
+            new webpack.BannerPlugin({
+                banner: `Geolocator.js https://github.com/onury/geolocator\n@license MIT. © ${new Date().getFullYear()}, Onur Yıldırım`,
+                raw: false
+            })
+        ],
+        // https://webpack.js.org/configuration/dev-server/#devserver
+        devServer: {
+            // Tell the server where to serve content from. This is only
+            // necessary if you want to serve static files. devServer.publicPath
+            // will be used to determine where the bundles should be served
+            // from, and takes precedence.
+            contentBase: path.resolve(__dirname, 'example'),
+            index: 'index.html',
+            // enable gzip compression for everything served
+            compress: true,
+            host: 'localhost',
+            port: 9991,
+            publicPath: '/' + PUBLIC_PATH,
+            // when using the HTML5 History API, the index.html page will likely
+            // have to be served in place of any 404 responses.
+            historyApiFallback: true,
+            // enable webpack's Hot Module Replacement feature
+            hot: true,
+            // Whether to embed the webpack-dev-server runtime into the bundle.
+            inline: false,
+            // if enabled, the dev-server will only compile the bundle when it
+            // gets requested. This means that webpack will not watch any file
+            // changes.
+            lazy: false,
+            // if enabled, messages like the webpack bundle information that is
+            // shown when starting up and after each save, will be hidden.
+            // errors and warnings will still be shown.
+            noInfo: true,
+            // if enabled, the dev server will open the browser.
+            open: false,
+            // openPage: '',
+            overlay: false,
+            quiet: false,
+            stats: 'normal',
+            watchContentBase: true,
+            watchOptions: {
+                aggregateTimeout: 300,
+                poll: 1000,
+                ignored: /node_modules/
             }
         }
     };
+
+    if (env.prod) {
+        config.mode = 'production';
+        config.output.filename = LIB_NAME + '.min.js';
+        config.optimization = {
+            minimize: true
+        };
+    }
+
+    if (env.serve) {
+        config.mode = 'development';
+        config.watch = true;
+        config.plugins.push(new webpack.HotModuleReplacementPlugin());
+    }
+
+    return config;
+
 };
