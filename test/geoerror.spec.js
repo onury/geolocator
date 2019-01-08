492732cc06b9f7d94a4c6c936b64e07e9f8f04a9
Onur Yıldırım <onur@cutepilot.com>
2019-01-09 02:51:46 +0300

    Update geoerror.spec.js

--- test/geoerror.spec.js
+++ test/geoerror.spec.js
@@ -1,25 +1,26 @@
 /* eslint no-console:1 */
-/* globals geolocator:false */
 
 'use strict';
 
+const geolocator = require('../dist/geolocator');
+
 describe('geolocator.Error', function () {
-    var err,
-        GeoError = geolocator.Error;
+    let err;
+    const GeoError = geolocator.Error;
 
     // beforeEach(function () {});
     // afterEach(function () {});
 
-    it('should check error', function () {
+    test('GeoError', () => {
         err = new GeoError();
         expect(err instanceof Error).toEqual(true);
         expect(err instanceof GeoError).toEqual(true);
         expect(err.name).toEqual('GeoError');
-        expect(Object.prototype.toString.call(err)).toEqual('[object Object]'); // ???
-        // expect(err.stack).toEqual(jasmine.any(String));
+        // expect(Object.prototype.toString.call(err)).toEqual('[object Object]');
+        // expect(err.stack).toEqual(expect.any(String));
     });
 
-    it('should check error code', function () {
+    test('error code', () => {
         expect(err.code).toEqual(GeoError.Code.UNKNOWN_ERROR);
         err = new GeoError(GeoError.Code.INVALID_PARAMETERS);
         expect(err.code).toEqual(GeoError.Code.INVALID_PARAMETERS);
